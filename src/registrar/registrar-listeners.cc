dab7be74a7afdad7690868ad135bf8f5bef22f76
Greeg Alton <galton@whitelabelcomm.com>
2025-04-30 11:07:06 -0500

    fix: Correct include path in registrar-listeners.cc

--- src/registrar/registrar-listeners.cc
+++ src/registrar/registrar-listeners.cc
@@ -2,7 +2,7 @@
  *  SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-#include "include/flexisip/registrar/registrar-listeners.hh"
+#include "registrar/registrar-listeners.hh"
 
 namespace flexisip {
 
